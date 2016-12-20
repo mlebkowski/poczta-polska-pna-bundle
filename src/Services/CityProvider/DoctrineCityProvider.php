553e0946a44769efbc342e04628f12a485c75b32
Maciej Łebkowski <m.lebkowski@gmail.com>
2016-12-20 11:54:40 +0100

    fix non-unique name/postcode combinations

--- src/Services/CityProvider/DoctrineCityProvider.php
+++ src/Services/CityProvider/DoctrineCityProvider.php
@@ -2,6 +2,7 @@
 
 namespace Nassau\PocztaPolskaPnaBundle\Services\CityProvider;
 
+use Doctrine\ORM\NonUniqueResultException;
 use Nassau\PocztaPolskaPnaBundle\Entity\CityInterface;
 use Nassau\PocztaPolskaPnaBundle\Entity\PnaCityInterface;
 use Nassau\PocztaPolskaPnaBundle\Entity\PnaCityRepository;
@@ -41,13 +42,25 @@ class DoctrineCityProvider implements CityProviderInterface
      */
     public function findByNameAndCode($name, $code)
     {
-        return $this->repository->createQueryBuilder('city')
-            ->leftJoin('city.streets', 'street')
-            ->where('city.name = :name')
-            ->setParameter('name', $name)
-            ->andWhere('street.code = :code OR city.code = :code')
-            ->setParameter('code', $code)
-            ->getQuery()
-            ->getOneOrNullResult();
+        try {
+            return $this->repository->createQueryBuilder('city')
+                ->leftJoin('city.streets', 'street')
+                ->where('city.name = :name')
+                ->setParameter('name', $name)
+                ->andWhere('street.code = :code OR city.code = :code')
+                ->setParameter('code', $code)
+                ->getQuery()
+                ->getOneOrNullResult();
+        } catch (NonUniqueResultException $e) {
+            //
+            // there may be in fact two different cities with the same name, and same post code. vide:
+            //
+            //  city_id | name      | commune   | county      | province    | city_code | street_id | code
+            // ---------+-----------+-----------+-------------+-------------+-----------+-----------+--------
+            //  55898   | Antoninów | Gostynin  | gostyniński | mazowieckie | NULL      | 58543     | 09-520
+            //  55900   | Antoninów | Łąck      | płocki      | mazowieckie | 09-520    | NULL      | NULL
+            //
+            return null;
+        }
     }
 }
